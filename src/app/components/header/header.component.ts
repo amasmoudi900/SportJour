c590a4b1951ffa03609bd71ca9dacb4347a3a25f
Abderrahmen Masmoudi <amasmoudi90@gmail.com>
2020-09-02 11:59:16 +0200

    Change var value

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   dateNow:Date;
-  name: string = 'salim';
+  name: string = 'Abderrahmen';
   constructor() { }
 
   ngOnInit() {
